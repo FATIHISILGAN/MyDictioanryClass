9d21bac39db9ec88bd503de327d8956f13366912
Fatih Işılgan <fatihisilgan@hotmail.com>
2021-01-29 22:40:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            MyDictionaryClass<string> myGeneric = new  MyDictionaryClass<string>();
+
+            myGeneric.Add("string değer");
+
+
+
+             MyDictionaryClass<int> myGeneric1 = new  MyDictionaryClass<int>();
+
+            int deger = 2;
+            myGeneric1.Add(deger);
+
+        }
+    }
+}
