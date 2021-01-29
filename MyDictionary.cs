829f561b0d6b80ab8e7cc7d7e7d6bdcae3a1a3c8
Fatih Işılgan <fatihisilgan@hotmail.com>
2021-01-29 22:44:54 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsExample
+{
+    class MyDictionary<TKey,TValue> //Dictioanry class 
+    {
+
+        TValue[] values;
+        TKey[] keys;
+
+        TKey[] temp_keys;
+        TValue[] temp_values;
+        public MyDictionary()
+        {
+            values = new TValue[0];
+            keys = new TKey[0];
+           
+        }
+        public void Add(TKey Key, TValue Value)
+        {
+            temp_keys = keys;
+            temp_values = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < temp_keys.Length; i++)
+            {
+                keys[i] = temp_keys[i];
+
+            }
+
+            for (int i = 0; i < temp_values.Length; i++)
+            {
+                values[i] = temp_values[i];
+            }
+
+
+
+            Console.WriteLine(" Anahtarların sayısı: " + keys.Length + " Değerin sayısı: " + values.Length);
+            Console.WriteLine("key no: " + Key + " olan " + Value + "  sisteme eklendi");
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace GenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(2, "fatih");
+
+
+
+
+
+            MyDictionary<string, string> myDictionary2 = new MyDictionary<string, string>();
+
+            myDictionary2.Add("car", "araba");
+
+        }
+    }
+}
